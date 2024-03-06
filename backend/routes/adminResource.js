2990dde8387352fb32c4ed5ac41601c23505094a
Basil <basiljiji2015@gmail.com>
2024-03-07 02:56:07 +0530

    Accidently Deleted Route Retrieved For Resource Fetch

--- backend/routes/adminResource.js
+++ backend/routes/adminResource.js
@@ -7,6 +7,7 @@ const router = express()
 
 //Resource
 router.get('/', authenticateAdmin, getAllFolders)
+router.get('/:folderName', authenticateAdmin, getResourceByFolder)
 router.post('/', authenticateAdmin, addFolder)
 router.post('/upload/:folderName', authenticateAdmin, upload.single('file'), uploadFile)
 router.patch('/folder/rename/:id', authenticateAdmin, renameFolder)
