6998ac447e10aafdce46d5c7cb1875dbb66c683a
Basil Jiji <basiljiji2015@gmail.com>
2024-04-29 02:04:32 +0530

    Paginate Overflow Fix

--- frontend/src/components/Paginate.jsx
+++ frontend/src/components/Paginate.jsx
@@ -1,21 +1,43 @@
-import { Pagination } from "react-bootstrap"
+import { Pagination, Container, Row } from "react-bootstrap"
 import { LinkContainer } from "react-router-bootstrap"
 
 const Paginate = ({ pages, page }) => {
+    const totalPages = Math.ceil(pages)
+    const currentPage = Math.min(Math.max(1, page), totalPages)
+    const startPage = Math.floor((currentPage - 1) / 10) * 10 + 1
+    const endPage = Math.min(startPage + 9, totalPages)
+
     return (
-        pages > 1 && (
-            <Pagination>
-                {[...Array(pages).keys()].map((x) => (
-                    <LinkContainer
-                        key={x + 1}
-                        to={`/admin/period/${x + 1}`}
-                    >
-                        <Pagination.Item active={x + 1 === page}>{x + 1}</Pagination.Item>
-                    </LinkContainer>
-                ))}
-            </Pagination>
+        totalPages > 1 && (
+            <Container className="d-flex justify-content-center">
+                <Row>
+                    <Pagination>
+                        {startPage > 1 && (
+                            <LinkContainer to={`/admin/period/${startPage - 1}`}>
+                                <Pagination.Prev />
+                            </LinkContainer>
+                        )}
+                        {[...Array(endPage - startPage + 1).keys()].map((index) => (
+                            <LinkContainer
+                                key={startPage + index}
+                                to={`/admin/period/${startPage + index}`}
+                            >
+                                <Pagination.Item active={startPage + index === currentPage}>
+                                    {startPage + index}
+                                </Pagination.Item>
+                            </LinkContainer>
+                        ))}
+                        {endPage < totalPages && (
+                            <LinkContainer to={`/admin/period/${endPage + 1}`}>
+                                <Pagination.Next />
+                            </LinkContainer>
+                        )}
+                    </Pagination>
+                </Row>
+            </Container>
         )
     )
-}
+};
+
 
 export default Paginate
